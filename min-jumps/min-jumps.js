df7671c909288cd0895726494fcb2ebe962cbab0
richmcateer <rmcateer1@gmail.com>
2020-02-19 17:42:12 -0500

    Finish min jumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -3,23 +3,17 @@
 // Complete this algo
 const minJumps = arr => {
   let jumpMap = { 0: 0 }; // Index => Steps
-  // For each index in the array
   for (let i = 0; i < arr.length; i++) {
-    // Get the element at that index
     let num = arr[i];
-    // Iterate element # of times
     for (let j = i + 1; j < i + num + 1; j++) {
-      // If you don't see the index in the cache
       if (!jumpMap[j]) {
-        const next = jumpMap[i] + 1;
+        const newJump = jumpMap[i] + 1;
 
-        // If we get to the end of the array, return the jump count at that index
         if (j === arr.length - 1) {
-          return next;
+          return newJump;
         }
 
-        // Otherwise add to cache
-        jumpMap[j] = next;
+        jumpMap[j] = newJump;
       }
     }
   }
